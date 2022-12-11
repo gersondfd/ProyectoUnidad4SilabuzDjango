2296abd856b6cf0feed132b5d33e8b73f395fc8f
gersondbg <gersondbg@gmail.com>
2022-12-10 23:14:44 -0500

    config db

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'EQUFg0H3zFVQyFmbuAlm',
+        'HOST': 'containers-us-west-38.railway.app',
+        'PORT': '6529',
+
     }
 }
 
